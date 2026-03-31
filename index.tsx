29cfce2a2fb0afba1445c92f150c79a2b6e5b80b
Emanuelly168 <manulima4333@gmail.com>
2026-03-31 13:27:09 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,20 +1,14 @@
 import { View, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-export default function HomeScreen() {
+export default function Home() {
   return (
     <View style={styles.container}>
-      {}
-      <Ionicons name="happy-outline" size={100} color="black" />
+      <Ionicons name="happy-outline" size={100} color="black" /> 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1, // 
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' },
 });
